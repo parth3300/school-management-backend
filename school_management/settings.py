22c40d82a3e84a9f9f3c7b289bf6097e4e6a0dbc
parth3300 <1111singalparth@gmail.com>
2025-05-11 20:39:32 +0530

    Fixed env error

--- school_management/settings.py
+++ school_management/settings.py
@@ -13,8 +13,10 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 import os
 from datetime import timedelta
-from dotenv import load_dotenv
-load_dotenv() 
+if os.getenv("ENV") != "production":
+    from dotenv import load_dotenv
+    load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
